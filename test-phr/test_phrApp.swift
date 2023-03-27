e6a9f8a4839d16a76f9f4ecff18ff5b096c22638
tunglampd <lam-pdt@genkisystem.com>
2023-03-27 10:18:43 +0700

    Initial Commit

---
+++ test-phr/test_phrApp.swift
@@ -0,0 +1,17 @@
+//
+//  test_phrApp.swift
+//  test-phr
+//
+//  Created by phan dam tung lam on 27/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct test_phrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
